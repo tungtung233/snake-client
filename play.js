5c2c1eaceacd08f811150656f1735f22c540dbea
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-30 16:59:25 +0000

    initial client code setup

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  conn.on("connect", () => {
+    console.log('hello')
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
